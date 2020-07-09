9a4ebc8758ef0f0b47e39afd3ed4ef2fb7eb2880
Philip Chislou <phliplviii@gmail.com>
2020-07-09 21:34:43 +0800

    Finished working on responsive column grids.

--- src/containers/Features/Features.js
+++ src/containers/Features/Features.js
@@ -5,7 +5,7 @@ import { FaArrowUp } from "react-icons/fa";
 const Features = (props) =>  {
 
     return (
-        <div>
+        <React.Fragment>
             <section className = 'section-features'>
                 <div className="row">
                     <h2>Get food fast &mdash; not fast food</h2>
@@ -13,29 +13,29 @@ const Features = (props) =>  {
                         Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!
                     </p>
                 </div>
-                <div className="row">
-                    <div className = 'span-1-of-4 box'>
+                <div className="wrapper">
+                    <div className = 'col span-1-of-4 box'>
                         <FaArrowUp className = 'icon-big'/>
                         <h3>Up to 365 days/year</h3>
                         <p> 
                             Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that's your style.
                         </p>
                     </div>
-                    <div className = 'span-1-of-4 box'>
+                    <div className = 'col span-1-of-4 box'>
                         <FaArrowUp className = 'icon-big'/>
                         <h3>Up to 365 days/year</h3>
                         <p> 
                             Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that's your style.
                         </p>
                     </div>
-                    <div className = 'span-1-of-4 box'>
+                    <div className = 'col span-1-of-4 box'>
                         <FaArrowUp className = 'icon-big'/>
                         <h3>Up to 365 days/year</h3>
                         <p> 
                             Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that's your style.
                         </p>
                     </div>
-                    <div className = 'span-1-of-4 box'>
+                    <div className = 'col span-1-of-4 box'>
                         <FaArrowUp className = 'icon-big'/>
                         <h3>Up to 365 days/year</h3>
                         <p> 
@@ -44,7 +44,7 @@ const Features = (props) =>  {
                     </div>
                 </div>
             </section>
-        </div>
+        </React.Fragment>
     );
 };
 
