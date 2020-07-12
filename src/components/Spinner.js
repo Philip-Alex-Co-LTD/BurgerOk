3e66d7452c904021eb769de5c18713c376f1e25f
Aleksey Novikov <alexx.novikovv@gmail.com>
2020-07-12 11:18:16 +0800

    Improved Spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
 const spinner = () => (
-    <div className="loader">Loading...</div>
+  <div>
+    <h3>Loading...</h3>
+    <div className="sk-cube-grid">
+      <div class="sk-cube1 sk-cube"></div>
+      <div class="sk-cube2 sk-cube"></div>
+      <div class="sk-cube3 sk-cube"></div>
+      <div class="sk-cube4 sk-cube"></div>
+      <div class="sk-cube sk-cube5"></div>
+      <div class="sk-cube sk-cube6"></div>
+      <div class="sk-cube sk-cube7"></div>
+      <div class="sk-cube sk-cube8"></div>
+      <div class="sk-cube sk-cube9"></div>
+    </div>
+  </div>
 );
-
 
 export default spinner;
