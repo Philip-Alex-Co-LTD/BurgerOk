08d3595d7a3951fe3489036de4eea53d53aec9bd
Aleksey Novikov <alexx.novikovv@gmail.com>
2020-07-27 00:20:37 +0800

    Gif picture with website order steps + responsive CSS

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,26 +1,32 @@
 import React from "react";
+import Img from "../assets/images/burger.gif";
 export default function HowItWorks() {
   return (
-    <React.Fragment>
-      <section className="section-steps" id="works">
-        <div className="row">
-          <h2>How it works &mdash; Simple as 1, 2, 3</h2>
-        </div>
-        <ol className = "span-1-of-3 how-it-works-steps">
+    <section className="section-steps" id="works">
+      <div className="row">
+        <h2>How it works &mdash; Simple as 1, 2, 3</h2>
+      </div>
+      <div className="section-works">
+        <ol className="span-1-of-3 how-it-works-steps">
           <li>
-            Login or create new account.<br/> Now you are ready to start! 
+            Login or create new account.
+            <br /> Now you are ready to start!
           </li>
           <li>
-            Build your own burger using any ingredient you like.<br/> Share with your friends!
+            Build your own burger using any ingredient you like.
+            <br /> Share with your friends!
           </li>
           <li>
-            Fill in the contact form & submit.<br/> Check out your order in "orders" section! 
+            Fill in the contact form & submit.
+            <br /> Check out your order in "orders" section!
           </li>
           <li>
-            Enjoy your meal after less than 20 minutes.<br/> See you next time!
+            Enjoy your meal after less than 20 minutes.
+            <br /> See you next time!
           </li>
         </ol>
-      </section>
-    </React.Fragment>
+        <img src={Img} alt="How It Works" className="img" />
+      </div>
+    </section>
   );
 }
